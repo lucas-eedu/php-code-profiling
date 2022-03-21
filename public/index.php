6ce769a6e04e4078b9f5bedf344f3fd181095dbf
Lucas Eduardo <lucas01.dev@gmail.com>
2022-03-21 16:41:16 -0300

    Initial Bot

---
+++ public/index.php
@@ -0,0 +1,21 @@
+<?php
+   $file = fopen('files/file-name.txt','r'); // Open the file
+   if ($file == false) die('Could not open file.');
+
+   $i = 0;
+   $arrFile = [];
+   
+   while(true) {
+      $line = fgets($file); // Cycling through each line of the file
+      if ($line == null) break;
+      $line = substr($line, 55); // Get only the text from the 55th line
+      $line = str_replace(array('{', '}', '[', ']', '"'), '', $line); // Removes the characters: {,},[,] and " -> No substitution
+      $line = str_replace(array(' ', '::', ':'), ',', $line); // Removes the characters: space,::, : and replace with ","
+      $arrFile[$i] = $line; // Assigning the text of the respective line to the array at its position
+      $i++;
+   }
+
+   $resultFile = fopen('oficial-files/file-name.txt','w'); // Create the file
+   fwrite($resultFile, implode($arrFile)); // Write to file (But first I transform the array into a string)
+
+   fclose($file); // Close the file
